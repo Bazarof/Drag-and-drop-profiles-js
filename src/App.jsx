88ae4f0f75ea876d3af46ae6a06b20e4f21f7fa6
Juan Jose Baza <baza_1546@outlook.com>
2024-06-09 22:49:37 -0600

    Content added to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,33 +3,121 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+//Profile photos
+import jamesPhoto from './assets/james.jpg'
+import alexPhoto from './assets/alex.jpg'
+import liliaPhoto from './assets/liliya.jpg'
+import melissaPhoto from './assets/melissa.jpg'
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+//Social icons
+import x from './assets/x.svg'
+import github from './assets/github.svg'
+import instagram from './assets/instagram.svg'
+import linkedin from './assets/linkedin.svg'
+
+function Profiles() {
+
+    const people = [
+        {
+            name: "James Alexander",
+            job: "Full Stack developer",
+            src: jamesPhoto,
+            alt: "James photo"
+        },
+        {
+            name: "Lilia Williams",
+            job: "UX/UI Designer",
+            src: alexPhoto,
+            alt: "Alex photo"
+        },
+        {
+            name: "Melissa Taylor",
+            job: "Data Science",
+            src: liliaPhoto,
+            alt: "Lilia photo"
+        },
+        {
+            name: "Alexander Smith",
+            job: "Product Manager",
+            src: melissaPhoto,
+            alt: "Melissa photo"
+        },
+    ];
+
+    const listPeople = people.map(
+        (e) => {
+            return (
+                <article className="team-member">
+                    <img
+                        className="team-member-avatar"
+                        src={e.src}
+                        alt={e.alt}
+                    />
+                    <div className="team-memeber-name">
+                        <h3>{e.name}</h3>
+                        <p>{e.job}</p>
+                    </div>
+                    <ul className="social-links">
+                        <li>
+                            <a href="#">
+                                <img
+                                    src={linkedin}
+                                    alt="Linkedin"
+                                />
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <img
+                                    src={instagram}
+                                    alt="Instagram"
+                                />
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <img
+                                    src={x}
+                                    alt="X"
+                                />
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <img
+                                    src={github}
+                                    alt="Github"
+                                />
+                            </a>
+                        </li>
+                    </ul>
+                </article>
+            );
+        }
+    );
+
+    return (
+        <div className="team-members">
+            {listPeople}
+        </div>
+    );
+}
+
+function Hola() {
+    return (
+        <>
+            <h1>Hola mundo</h1>
+        </>
+    );
+}
+
+function App() {
+
+    return (
+        <>
+            <Profiles />
+        </>
+    );
 }
 
 export default App
